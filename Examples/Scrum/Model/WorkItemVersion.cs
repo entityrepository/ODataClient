cb743af439ede9b2a56d8c55af81ee89a08fd8c2
john.crim <john.crim@fba3dbb8-ca94-cd4a-bcfd-df4502d74e4b>
2014-07-25 00:17:16 +0000

    Inconsequential change: Updating sample to work with external EntityRepository server sample.

---
+++ Examples/Scrum/Model/WorkItemVersion.cs
@@ -0,0 +1,64 @@
+﻿// // -----------------------------------------------------------------------
+// <copyright file="WorkItemVersion.cs" company="PrecisionDemand">
+// Copyright (c) 2014 PrecisionDemand.  All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+
+using PD.Base.PortableUtil.Model;
+
+namespace Scrum.Model
+{
+
+	/// <summary>
+	/// An example entity with multiple keys - just to test the "more than one PK column" case.
+	/// </summary>
+	public sealed class WorkItemVersion
+	{
+
+		private EntityRef<WorkItem, int> _workItem = new EntityRef<WorkItem, int>(workItem => workItem.ID);
+
+		public WorkItemVersion(WorkItem workItem, byte version)
+		{
+			WorkItem = workItem;
+			Version = version;
+		}
+
+		/// <summary>
+		/// Useful for deserialization.
+		/// </summary>
+		public WorkItemVersion()
+		{
+		}
+
+		public WorkItem WorkItem
+		{
+			get
+			{
+				return _workItem.Entity;
+			}
+			set
+			{
+				_workItem.Entity = value;
+			}
+		}
+		public int WorkItemId
+		{
+			get
+			{
+				return _workItem.ForeignKey;
+			}
+			set
+			{
+				_workItem.ForeignKey = value;
+			}
+		}
+
+		public byte Version
+		{
+			get;
+			set;
+		}
+	}
+
+}

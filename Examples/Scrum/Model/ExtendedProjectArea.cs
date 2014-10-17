80d1931818a726a9b555117e4d2e78f8d5772f42
nparrish <nparrish@fba3dbb8-ca94-cd4a-bcfd-df4502d74e4b>
2014-10-17 04:45:28 +0000

    BENTREPO-21 Adding missing file

---
+++ Examples/Scrum/Model/ExtendedProjectArea.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ExtendedProjectArea.cs" company="Adap.tv">
+// Copyright (c) 2014 Adap.tv.  All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Scrum.Model
+{
+    public class ExtendedProjectArea : ProjectArea
+    {
+
+        public string Tag { get; set; }
+
+    }
+}

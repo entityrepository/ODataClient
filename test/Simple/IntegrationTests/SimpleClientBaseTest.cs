314afb0d2287d0d3a2664288a961433869eadc89
Nathan Parrish <nathaniel.parrish@teamaol.com>
2015-09-25 09:16:10 -0700

    Moved Simple integration tests

---
+++ test/Simple/IntegrationTests/SimpleClientBaseTest.cs
@@ -0,0 +1,35 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SimpleClientBaseTest.cs" company="PrecisionDemand">
+// Copyright (c) 2013 PrecisionDemand.  All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Xunit;
+
+namespace Simple.IntegrationTests
+{
+
+	public abstract class SimpleClientBaseTest
+	{
+
+		// TODO: Shorten the timeout for real testing to 4000 or so
+		internal const int TestTimeout = 600000; // For debugging, this is 10m
+
+		private readonly SimpleClient _client;
+
+		protected SimpleClientBaseTest()
+		{
+			_client = new SimpleClient(SimpleClient.TestServiceUrl);
+
+			// REVIEW: We should require that this is called before any repository properties are accessed.  But that's somewhat difficult to do,
+			// so for now, we have to wait for initialization to complete.
+			Assert.True(_client.InitializeTask.Wait(TestTimeout));
+		}
+
+		protected SimpleClient Client
+		{
+			get { return _client; }
+		}
+
+	}
+}
---
+++ test/Simple/IntegrationTests/TestEqualityQueries.cs
@@ -0,0 +1,88 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="TestEqualityQueries.cs" company="PrecisionDemand">
+// Copyright (c) 2013 PrecisionDemand.  All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using PD.Base.EntityRepository.ODataClient;
+using Simple.Model;
+using Xunit;
+
+namespace Simple.IntegrationTests
+{
+
+	/// <summary>
+	/// Exercises querying of <see cref="EqualityTestRecord"/>s from the server, with the intent of directing the
+	/// requirements for implementing equality in our entities.
+	/// </summary>
+	public class TestEqualityQueries : SimpleClientBaseTest
+	{
+
+		private IEnumerable<EqualityTestRecord> QueryForRecordsWith(EqualitySemantics equalitySemantic, int expectedRecordCount)
+		{
+			var query = Client.EqualityTestRecords.Include(r => r.EqualitySemantic).Where(r => r.EqualitySemantic.ID == equalitySemantic.ID);
+			Assert.True(Client.InvokeAsync(query).Wait(TestTimeout));
+
+			// Verify expected record count.
+			Assert.Equal(expectedRecordCount, query.Count());
+
+			// Verify that the DbEnum value was pulled into the ReadOnlyRepository - even though it was included and not the repository that was queried
+			Assert.Contains(equalitySemantic, Client.EqualitySemantics.Local);
+
+			return query;
+		}
+
+		[Fact]
+		public void QueryWithIdentityEqualityPreservesChanges()
+		{
+			var results = QueryForRecordsWith(EqualitySemantics.IdentityOnly, 2).ToArray();
+			results[0].Payload += "-modified";
+			var results2 = QueryForRecordsWith(EqualitySemantics.IdentityOnly, 2).ToArray();
+
+			Assert.Equal(results, results2);
+
+			// Note that identity equality preserves changes - which it should, since we use MergeOption.PreserveChanges
+			Assert.True(results[0].Payload.EndsWith("-modified"));
+		}
+
+		[Fact]
+		public void ValueEqualityThrowsExceptionWhenEntityValuesAreEqual()
+		{
+			// This exception occurs if we use value equality and two objects are the same (same hash and Equals == true):
+			//
+			//System.InvalidOperationException : The context is already tracking a different entity with the same resource Uri.
+			//   at System.Data.Services.Client.EntityTracker.InternalAttachEntityDescriptor(EntityDescriptor entityDescriptorFromMaterializer, Boolean failIfDuplicated)
+			//   at System.Data.Services.Client.AtomMaterializerLog.ApplyToContext()
+			//   at System.Data.Services.Client.Materialization.ODataEntityMaterializer.ApplyLogToContext()
+			//   at System.Data.Services.Client.MaterializeAtom.MoveNextInternal()
+			//   at System.Data.Services.Client.MaterializeAtom.MoveNext()
+			//   at System.Linq.Enumerable.<CastIterator>d__b1`1.MoveNext()
+			//   at System.Linq.Buffer`1..ctor(IEnumerable`1 source)
+			//   at System.Linq.Enumerable.ToArray[TSource](IEnumerable`1 source)
+			//   at PD.Base.EntityRepository.ODataClient.EditRepository`1.ProcessQueryResults(IEnumerable`1 entities) in c:\src\code\Base\EntityRepository\trunk\ODataClient\EditRepository.cs:line 45
+			//   at PD.Base.EntityRepository.ODataClient.ODataClient.ProcessQueryResults[TEntity](IEnumerable`1 results) in c:\src\code\Base\EntityRepository\trunk\ODataClient\ODataClient.cs:line 619
+			//   at PD.Base.EntityRepository.ODataClient.ODataClientQuery`1.HandleResponse(ODataClient client, OperationResponse operationResponse) in c:\src\code\Base\EntityRepository\trunk\ODataClient\ODataClientQuery.cs:line 209
+			Assert.Throws<InvalidOperationException>(() => QueryForRecordsWith(EqualitySemantics.ValuesOnly, 2));
+		}
+
+		[Fact(Skip = "This test worked until I added the cached hash code implementation.")]
+		public void QueryWithIdentityAndValueEqualityLosesChanges()
+		{
+			var results = QueryForRecordsWith(EqualitySemantics.IdentityAndValues, 2).ToArray();
+			results[0].Payload += "-modified";
+			var results2 = QueryForRecordsWith(EqualitySemantics.IdentityAndValues, 2).ToArray();
+
+			Assert.Equal(results, results2);
+			Assert.Equal(results[0].Payload, results2[0].Payload);
+
+			// If we use both identity and value equality in Equals() and GetHashCode(),
+			// changes to entities are overwritten.  Presumably that's because the identity is the same,
+			// but the object is different, so it's overwritten.
+			Assert.False(results[0].Payload.EndsWith("-modified"));
+		}
+
+	}
+}

d982e4c3eaf54596242bbe6b1479893a1e872a84
Nathan Parrish <nathaniel.parrish@teamaol.com>
2015-09-25 09:15:45 -0700

    Moved Scrum integration tests

---
+++ src/ODataClient/EntityValidation.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="EntityValidation.cs" company="AOL">
+// Copyright (c) 2015 AOL Platforms.  All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Microsoft.Data.Edm.Validation;
+
+namespace EntityRepository.ODataClient
+{
+
+	/// <summary>
+	/// Container class for entity validation data
+	/// </summary>
+	public static class EntityValidation
+	{
+
+		/// <summary>
+		/// A key for <see cref="ValidationContext.Items"/>, indicating whether the entity being validated was added to the data context or not.
+		/// </summary>
+		public const string IsAddedKey = "IsAdded";
+
+		/// <summary>
+		/// A key for <see cref="ValidationContext.Items"/>, indicating whether the entity being validated was modified or not.
+		/// </summary>
+		public const string IsModifiedKey = "IsModified";
+
+	}
+}
